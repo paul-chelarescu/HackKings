9d76f95f89f72ed1bf2171b08ff633f66a1bc7c2
Akiba12 <Natalie@MacBook-Pro-Natalie.local>
2015-12-13 09:33:22 +0000

    added w-a-s-d

--- js/app.js
+++ js/app.js
@@ -38,6 +38,38 @@ btn.addEventListener("click", function(){ drawModule.init();});
           }
           break;
           }
+
+        case 87:
+          if (direction != 'down'){
+            direction = 'up';
+            console.log('up');
+          }
+          break;
+          }
+
+        case 65:
+          if (direction != 'right'){
+            direction = 'left';
+            console.log('left');
+          }
+          break
+          }
+
+        case 83:
+          if (direction != 'up'){
+            direction = 'down';
+            console.log('down');
+          }
+          break
+          }
+
+         case 68:
+          if (direction != 'left'){
+            direction = 'right';
+            console.log('right');
+          }
+          break
+          }        
       }
 
 
